ca0abc68ccf39dc4aaa83bc17d668eb4466738d8
Kim Sungjin <kimxungjin@icloud.com>
2021-05-18 00:32:04 +0900

    12일차 - Chapter05 앱재부팅시 selectedAccount 불러오기까지 진행

---
+++ Chapter05-CustomPlist/Chapter05-CustomPlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter05-CustomPlist
+//
+//  Created by 김성진 on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - ✅ Chapter 02 - 4월 27일
 - ✅Chapter 03 - 4월 28, 29, 30일, 5월 1일
 - ✅Chapter 04 - ✅5월  2일, ✅3일, ✅6일
-- Chapter 05 - ✅5월 9일, ✅5월 14일
+- Chapter 05 - ✅5월 9일, ✅5월 14일, ✅5월 17일 
 - Chapter 06 - 
 - Chapter 07 - 
 - Chapter 08 - 
